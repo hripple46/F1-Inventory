8397ea310c872a96341b14de70fd0509ba3e848c
Henry Ripple <hripple46@gmail.com>
2023-05-21 11:14:13 -0500

    functioning skeleton structure

--- Controller/teamsController.js
+++ Controller/teamsController.js
@@ -5,3 +5,7 @@ exports.index = asyncHandler(async (req, res, next) => {
     title: "Formula 1",
   });
 });
+
+exports.teams_list = asyncHandler(async (req, res, next) => {
+  res.send("Not Implemented: Team Page ");
+});
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 var express = require("express");
-const router = express.Router();
+var router = express.Router();
 const teams_controller = require("../Controller/teamsController");
 
 /* GET home page. */
 router.get("/", teams_controller.index);
+
+router.get("/teams", teams_controller.teams_list);
 
 module.exports = router;
