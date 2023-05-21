02c5c198ed2b7cca64a3341201ab06049803eb4f
Henry Ripple <hripple46@gmail.com>
2023-05-21 14:29:02 -0500

    added schemas for inventory

---
+++ models/teamPrincipal.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const TeamPrincipalSchema = new Schema({
+  name: { type: String, required: true },
+  age: { type: Number, required: true },
+  team: { type: Schema.Types.ObjectId, ref: "Team", required: true },
+});
+
+TeamPrincipalSchema.virtual("url").get(function () {
+  return `/teamprincipal/${this._id}`;
+});
+
+module.exports = mongoose.model("TeamPrincipal", TeamPrincipalSchema);
