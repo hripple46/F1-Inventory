608b10c65984a0fbfa2988e0cc9c6a365b17d310
Henry Ripple <hripple46@gmail.com>
2023-05-22 16:26:22 -0500

    configured tailwind

--- Controller/teamsController.js
+++ Controller/teamsController.js
@@ -1,8 +1,11 @@
 const asyncHandler = require("express-async-handler");
+const Teams = require("../models/team");
 
 exports.index = asyncHandler(async (req, res, next) => {
+  const allTeams = await Teams.find().sort({ name: 1 }).exec();
   res.render("index", {
     title: "Formula 1",
+    teams_list: allTeams,
   });
 });
 
