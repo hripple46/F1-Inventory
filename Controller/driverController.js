15e5369cca1b6803277ff4a405dc122fa41a8dc3
Henry Ripple <hripple46@gmail.com>
2023-05-23 16:44:28 -0500

    added drivers list page and more styling

--- Controller/driverController.js
+++ Controller/driverController.js
@@ -1,5 +1,10 @@
 const asyncHandler = require("express-async-handler");
+const Driver = require("../models/driver");
 
 exports.driver_list = asyncHandler(async (req, res, next) => {
-  res.send("Not Implemented: Driver Page");
+  const AllDrivers = await Driver.find().sort({ name: 1 }).exec();
+  res.render("driver_list", {
+    driver_list: AllDrivers,
+    title: "F1 Drivers",
+  });
 });
